e76603f3fe7d449f75fbd9f6d8bd87170836a92c
ChrongZilla <rudals3620@naver.com>
2026-07-18 19:27:21 +0900

    feat: Main 작성 및 메모리 버전 CRUD 완성

---
+++ src/main/java/com/example/restaurant/Main.java
@@ -0,0 +1,17 @@
+package com.example.restaurant;
+
+import com.example.restaurant.repository.RestaurantRepository;
+import com.example.restaurant.repository.RestaurantMemoryRepository;
+import com.example.restaurant.service.RestaurantService;
+import com.example.restaurant.view.ConsoleView;
+
+public class Main {
+    public static void main(String[] args) {
+        RestaurantRepository repository = new RestaurantMemoryRepository();
+        // RestaurantRepository repository = new RestaurantFileRepository();
+        // RestaurantRepository repository = new RestaurantDBRepository();
+        RestaurantService service = new RestaurantService(repository);
+        ConsoleView view = new ConsoleView(service);
+        view.run();
+    }
+}
