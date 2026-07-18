f7be77a621e46508685c573bf13eeeaa12f828de
ChrongZilla <rudals3620@naver.com>
2026-07-18 22:55:02 +0900

    chore: Main에서 DB 버전으로 전환

--- src/main/java/com/example/restaurant/Main.java
+++ src/main/java/com/example/restaurant/Main.java
@@ -6,6 +6,7 @@ import com.example.restaurant.repository.RestaurantFileRepository;
 import com.example.restaurant.repository.RestaurantDBRepository;
 import com.example.restaurant.service.RestaurantService;
 import com.example.restaurant.view.ConsoleView;
+import com.example.restaurant.util.DBConnection;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,12 +14,14 @@ public class Main {
         System.out.println("[메모리 버전으로 실행 중입니다]\n");
         RestaurantRepository repository = new RestaurantMemoryRepository();
          */
+        /*
         System.out.println("[파일 버전으로 실행 중입니다]\n");
         RestaurantRepository repository = new RestaurantFileRepository();
-        /*
+         */
+
         System.out.println("[DB 버전으로 실행 중입니다]\n");
         RestaurantRepository repository = new RestaurantDBRepository();
-         */
+
         RestaurantService service = new RestaurantService(repository);
         ConsoleView view = new ConsoleView(service);
         view.run();
